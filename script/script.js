5035067c43852af0dcb3caa721d9f45a90fc7991
Chany4 <chandreleighdavids04052005@gmail.com>
2024-06-04 12:32:08 +0200

    added

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// Area of a triangle
+
+let base = document.querySelector('#base');
+let height= document.querySelector('#height');
+let btn = document.querySelector('#areaCon');
+let areaSpan = document.querySelector('#TriSpan')
+
+btn.addEventListener('click', ()=>{
+    areaSpan.innerHTML = [(0.5 *base.value * height.value) ]
+})
